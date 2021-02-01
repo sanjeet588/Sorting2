44b9af9f7af7c512e9d6394ed17c1ca2ff8bac1d
Sanjeet Nayak <sanjeetnayak@localhost>
2021-02-01 10:11:03 +0530

    New Sorting Algo

---
+++ src/NewSort2.java
@@ -0,0 +1,10 @@
+
+public class NewSort2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("New Sorting");
+		System.out.println("END");
+	}
+
+}
