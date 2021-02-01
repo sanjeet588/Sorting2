db6673cb378ef7fe0dfe80b083cbc68673b690a1
Sanjeet Nayak <sanjeetnayak@localhost>
2021-02-01 10:02:16 +0530

    bubble sort

---
+++ src/BubbleSort2.java
@@ -0,0 +1,10 @@
+
+public class BubbleSort2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bubble Sorting");
+		System.out.println("END");
+	}
+
+}
